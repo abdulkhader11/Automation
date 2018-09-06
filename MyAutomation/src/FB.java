b249dcb9e99f76f7cf24f26881596edfe11333cb
Abdul Khader <Abdul Khader@Abdul>
2018-09-06 17:51:21 +0530

    latest commit

--- MyAutomation/src/FB.java
+++ MyAutomation/src/FB.java
@@ -1,16 +1,46 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class FB {
-	public static void main(String[] args) 
-	{
-	System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-	WebDriver driver=new ChromeDriver();
-	driver.get("http://fb.com/");
-	driver.manage().window().maximize();
-	driver.findElement(By.xpath(".//*[@id='day']")).click();
-	driver.findElement(By.xpath(".//*[@id='day']/option[8]")).click();
+
 	
+	WebDriver driver= new ChromeDriver();
+	
+	
+	@BeforeTest
+	public void start() throws IOException, InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		
+	
+		driver.get("http://fb.com/");
+		Thread.sleep(1000);	
+		
 	}
-}
+	@Test
+	public void fb() throws InterruptedException {
+		Thread.sleep(7000);
+		driver.findElement(By.xpath("//input[@id='u_0_j']")).sendKeys("Anam");
+		driver.findElement(By.xpath(".//*[@id='u_0_l']")).sendKeys("Khan");
+		driver.findElement(By.xpath(".//*[@id='u_0_o']")).sendKeys("anam123khan@gmail.com");
+		driver.findElement(By.xpath(".//*[@id='u_0_r']")).sendKeys("anam123khan@gmail.com");
+		driver.findElement(By.xpath(".//*[@id='u_0_v']")).sendKeys("123anam");	
+	}
+	
+	@AfterTest
+	public void quit() {
+		
+		driver.quit();
+	}
+	}
+	
